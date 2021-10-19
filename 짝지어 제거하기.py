83a1b1a6da8b4b84ec601b01d8a73e548a3fac91
keumbong <87923413+keumbong@users.noreply.github.com>
2021-10-19 23:20:04 +0900

    Add files via upload

---
+++ 짝지어 제거하기.py
@@ -0,0 +1,10 @@
+def solution(s):
+    st = []
+    for i in s:
+        if len(st) == 0: st.append(i)
+        elif st[-1] == i: st.pop()
+        else: st.append(i)
+    if len(st) == 0:
+        return 1
+    else:
+        return 0
