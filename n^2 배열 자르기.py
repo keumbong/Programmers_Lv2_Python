6a26d548535aa26078b2957a36173bebb477a851
keumbong <87923413+keumbong@users.noreply.github.com>
2021-10-28 21:24:10 +0900

    Update n^2 배열 자르기.py

--- n^2 배열 자르기.py
+++ n^2 배열 자르기.py
@@ -7,3 +7,10 @@ def solution(n, left, right):
         arr_list += arr
     return arr_list[left:right+1]
 # 시간초과
+
+
+def solution(n, left, right):
+    answer = []    
+    for i in range(left, right + 1):
+        answer.append(max(divmod(i, n)) + 1)
+    return answer
